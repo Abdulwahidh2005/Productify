7e8539dd07d641e87873f2a6dd8d47069c83e654
Abdulwahdih2005 <abdulwahidh@2005>
2026-01-17 22:36:56 +0530

    feat: add LoadingSpinner component and ProductCard component
    - Created a LoadingSpinner component for displaying loading state with an animated icon.
    - Implemented a ProductCard component to showcase product details, including title, description, user info, and comment count.
    - Added a custom hook, useProducts, for fetching products and useCreateProduct for creating new products.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import Navbar from "./components/Navbar";
-import { Route,Routes } from "react-router";
+import { Navigate, Route,Routes } from "react-router";
 import HomePage from "./pages/HomePage";
 import EditProductPage from "./pages/EditProductPage";
 import ProductPage from "./pages/ProductPage"
@@ -23,7 +23,8 @@ function App() {
             <Route path="/" element={<HomePage />}/>
             <Route path="/product/:id" element={<ProductPage />}/>
             <Route path="/profile" element={<ProfilePage />}/>
-            <Route path="/create" element={<CreatePage />}/>
+            <Route path="/create" element={isSignedIn ? <CreatePage/> : <Navigate to={"/"}/>
+            }/>
             <Route path="/edit/:id" element={<EditProductPage />}/>
           </Routes>
         </main>
